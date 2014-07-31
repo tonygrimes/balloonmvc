68676009ef24c2ee94d401424e8c7e4897d58c7a
tony.grimes <tony.grimes@gamesys.co.uk>
2014-07-31 09:07:29 +0100

    mvc changes

--- webapp/scripts/TestMain.js
+++ webapp/scripts/TestMain.js
@@ -1,36 +1,74 @@
-require(['NotificationCentre', "CommsManager", "ParallaxView", "BalloonView", "utils"],
+require(['libs/NotificationCentre', "CommsManager", "ParallaxView", "BalloonView", "utils"],
 
 	function   (NotificationCentre, CommsManager, ParallaxView, BalloonView, utils) {
 
+		//******* PARENT class
 
-		//create parent obj	
-		var parentObj = {
+		console.log(NotificationCentre)
 
-			name : "tony",
-			age : 37,
-			sex : "male",
-			sayHello : function(){
-				return "Hi, my name is " + this.name + " and i am " + this.age;
-			}			
-			//return this;
-
+		function Parent (name) {
+			this.myname = "my name is "+name;	
 		}
 
-		//create child obj
-		var childObj = utils.create(parentObj);
-		childObj.name = "Leo";
-		childObj.age = 3;
-
-		parentObj.name="Bill";
-
-		//call super function and override it
-		childObj.sayHello = function(){
-
-			var message = parentObj.sayHello();
-			return message + " and i am " + this.name + " and i am a " + this.sex;
+		Parent.prototype.sayHello = function(){
+			return this.myname;
 		}
 
-		console.log(childObj.sayHello());
+		// Parent.prototype = {
+
+		// 	constructor : Parent, //this needed because sayHello's constructor is an Object NOT Parent
+		// 	sayHello : function(){
+		// 		//debugger;
+		// 		return this.myname;
+		// 	}
+
+		// }
+
+		//****** CHILD class 
+
+		utils.extends(Child, Parent);
+
+		function Child(name){
+			//Child.uber is a reference to Parent.prototype
+			//Child.uber.constructor is a reference to the Parent function which is the constructor of Parent.prototype
+			//we call the Parent construtor in the scope of Child
+			//Q: why does this.uber return undefined?
+			//this is an instance of Child 
+			//uber is defined as a property of the Child function
+			//so maybe this.constructor.uber (a reference to the Child function object itself) would work?
+			//A: YES. this.constructor.uber.constructor.apply(this, [name]) works as well
+			//Parent === Child.uber.constructor
+
+			Child.uber.constructor.apply(this, [name]);
+		}
+
+		Child.prototype.sayHello = function(){
+			//Because Parent is a function, we access its prototype through the prototype keyword. 
+			//You dont need to do this with Parent instances. 
+			//So Parent.protoype.sayHello or parentInstance.sayHello will work
+			
+			//Parent.prototype.sayHello.apply(this, [name]) is same as below
+			return Child.uber.sayHello.call(this);
+		}
+
+		//****** GRANDCHILD class 
+
+		utils.extends(GrandChild, Child);
+
+		function GrandChild(name){
+			GrandChild.uber.constructor.apply(this, [name]);
+		}
+
+		GrandChild.prototype.sayHello = function(){
+			return GrandChild.uber.sayHello.call(this) + " as Grandchild ";
+		}
+
+		//***** DO STUFF
+
+		var gchild = new Child("Leo");
+
+		console.log(gchild.sayHello());
+
 		
 	}
 );
--- webapp/scripts/utils.js
+++ webapp/scripts/utils.js
@@ -2,14 +2,57 @@ define(function(){
 	
 	var utils = {};
 
+
+	//in this version changes to parentObj affect all subclasses and child also gets instance propertoe sof parent
 	utils.create = function(parentObj){
 
 		function F(){};
 		F.prototype = parentObj;
-		var obj=new F();
-		return obj;
+		return new F();
 
 	};
+
+	//in this version you only inherit the prototype which should not contain instance properties
+	utils.extends = function(C, P){
+
+		function F(){};
+		F.prototype = P.prototype;
+
+		//why not this instead if we just want to have access to the prototype of P, and do away with F altogether?
+		//C.prototype = P.prototype;
+		//it is so that i cannot alter the prototype chain of P and thereby alter the inherited values of other objects
+
+		C.prototype = new F();
+		C.uber = P.prototype;
+		C.prototype.constructor = C;
+
+	};
+
+	//var Child = Child.extends(Parent)
+
+	utils.newClass = function(Parent, props){
+
+		var Child = function(){
+			if(Child.uber && Child.uber._init){
+				Child.uber._init.apply(this, [name]);
+			}
+		}
+
+		Parent = Parent || Object;
+
+		function F(){};
+		F.prototype = Parent.prototype;
+		Child.prototype = new F();
+		Child.uber = Parent.prototype;
+		Child.prototype.constructor = Child;
+
+		for(i in props){
+			Child.prototype[i] = props[i];
+		}
+
+		return Child;
+
+	}
 
 	return utils;
 
